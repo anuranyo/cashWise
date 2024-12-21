c9dbc6f3d4a807faa459696f7b744b9c801e735c
anuranyo <Sotnikoff05@gmail.com>
2024-12-21 22:45:43 +0200

    settings

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Stack, Stack as TabsStack } from 'expo-router';
+import { Stack, Tabs, Stack as TabsStack } from 'expo-router';
 import { TabProvider } from '../contexts/TabContext';
+import { FontAwesome5 } from '@expo/vector-icons';
 
 export default function TabsLayout() {
   return (
-    <TabProvider>
+    <TabProvider children={undefined}>
       <TabsStack screenOptions={{ headerShown: false }}>
         <TabsStack.Screen name="auth" options={{ title: 'Authorization' }} />
         <TabsStack.Screen name="index" options={{ title: 'Home' }} />
@@ -13,7 +14,7 @@ export default function TabsLayout() {
         <TabsStack.Screen name="signupSstep" options={{ title: 'Second Step' }} />
         <TabsStack.Screen name="signup" options={{ title: 'SignUp' }} />
         <TabsStack.Screen name="forgotPassword" options={{ title: 'Forgot Password' }} />
-
+        <Tabs.Screen name="SecurityScreen" options={{ tabBarIcon: () => <FontAwesome5 name="shield-alt" /> }}/>
 
       </TabsStack>
     </TabProvider>
